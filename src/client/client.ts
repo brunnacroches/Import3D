d53228c9ce43e0af40ac8ee68c93a0821fa4e2a6
brunnacroches <brunnacroches@gmail.com>
2021-12-04 00:50:45 -0300

    materiais parte 2

--- src/client/client.ts
+++ src/client/client.ts
@@ -26,10 +26,7 @@ const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0)
 const planeGeometry = new THREE.PlaneGeometry()
 const torusKnotGeometry = new THREE.TorusKnotGeometry()
 
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+const material = new THREE.MeshBasicMaterial()
 //const material= new THREE.MeshNormalMaterial()
 
 const cube = new THREE.Mesh(boxGeometry, material)
@@ -63,29 +60,33 @@ function onWindowResize() {
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
-// const options = {
-//     side: {
-//         "FrontSide": THREE.FrontSide,
-//         "BackSide": THREE.BackSide,
-//         "DoubleSide": THREE.DoubleSide,
-//     }
-// }
+const options = {
+    side: {
+        FrontSide: THREE.FrontSide,
+        BackSide: THREE.BackSide,
+        DoubleSide: THREE.DoubleSide,
+    },
+}
 
 const gui = new GUI()
 const materialFolder = gui.addFolder('THREE.Material')
-// materialFolder.add(material, 'transparent')
-// materialFolder.add(material, 'opacity', 0, 1, 0.01)
-// materialFolder.add(material, 'depthTest')
-// materialFolder.add(material, 'depthWrite')
-// materialFolder.add(material, 'alphaTest', 0, 1, 0.01).onChange(() => updateMaterial())
-// materialFolder.add(material, 'visible')
-// materialFolder.add(material, 'side', options.side).onChange(() => updateMaterial())
+materialFolder.add(material, 'transparent')
+materialFolder.add(material, 'opacity', 0, 1, 0.01)
+materialFolder.add(material, 'depthTest')
+materialFolder.add(material, 'depthWrite')
+materialFolder
+    .add(material, 'alphaTest', 0, 1, 0.01)
+    .onChange(() => updateMaterial())
+materialFolder.add(material, 'visible')
+materialFolder
+    .add(material, 'side', options.side)
+    .onChange(() => updateMaterial())
 materialFolder.open()
 
-// function updateMaterial() {
-//     material.side = Number(material.side)
-//     material.needsUpdate = true
-// }
+function updateMaterial() {
+    material.side = Number(material.side)
+    material.needsUpdate = true
+}
 
 function animate() {
     requestAnimationFrame(animate)
